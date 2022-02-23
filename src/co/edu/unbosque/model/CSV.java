88b541d2b2e6e34c24ea63f10b80b75806476b28
SantiagoMelo909 <santiagomelocristancho@gmail.com>
2022-02-23 16:38:56 -0500

    3 requerimento

--- src/co/edu/unbosque/model/CSV.java
+++ src/co/edu/unbosque/model/CSV.java
@@ -9,52 +9,50 @@ import java.util.List;
 public class CSV {
 
 	private List<Registro> csvList;
+//test
+	public CSV(String localpath) {
+		csvList = new ArrayList<>();
+		readFromPath(localpath);
+	}
 
-    public CSV(String localpath) {
-        csvList = new ArrayList<>();
-        readFromPath(localpath);
-    }
+	public void readFromPath(String localpath) {
+		System.out.println("Reading a CSV using OpenCSV library");
+		try {
+			CSVReader reader = new CSVReader(new FileReader(localpath));
+			String[] linea;
+			reader.readNext();
+			do {
+				linea = reader.readNext();
+				if (linea != null) {
+					try {
+						String invoiceNo = linea[0];
+						String stockCode = linea[1];
+						String description = linea[2];
+						String quantity = linea[3];
+						String invoiceDate = linea[4];
+						String unitPrice = linea[5];
+						String customerId = linea[6];
+						String country = linea[7];
+						Registro registro = new Registro(invoiceNo, stockCode, description, quantity, invoiceDate,
+								unitPrice, customerId, country);
+						csvList.add(registro);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			} while ((linea != null));
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void readFromPath(String localpath) {
-        System.out.println("Reading a CSV using OpenCSV library");
-        try {
-            CSVReader reader = new CSVReader(new FileReader(localpath));
-            String[] linea;
-            reader.readNext();
-            do {
-                linea = reader.readNext();
-                try {
-                    String invoiceNo    = linea[0];
-                    String stockCode    = linea[1];
-                    String description  = linea[2];
-                    String quantity     = linea[3];
-                    String invoiceDate  = linea[4];
-                    String unitPrice    = linea[5];
-                    String customerId   = linea[6];
-                    String country      = linea[7];
-                    Registro registro   = new Registro(invoiceNo, stockCode, description, quantity, invoiceDate, unitPrice, customerId, country);
-                    csvList.add(registro);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } while ((linea != null));
-            reader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public List<Registro> getCsvList() {
+		return csvList;
+	}
 
-    public List<Registro> getCsvList() {
-        return csvList;
-    }
+	public void setCsvList(List<Registro> csvList) {
+		this.csvList = csvList;
+	}
 
-    public void setCsvList(List<Registro> csvList) {
-        this.csvList = csvList;
-    }
-	
-	
-	
-	
-	
-	
 }
