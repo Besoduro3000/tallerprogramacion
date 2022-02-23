5e1b556c7116a7416bc4a9d86e9e4f98a63d4bd7
Besoduro3000 <pedrerosc0502@gmail.com>
2022-02-23 14:57:01 -0500

    agregacion de la vista y empiezo con el 3cer requerimento

--- src/co/edu/unbosque/view/View.java
+++ src/co/edu/unbosque/view/View.java
@@ -1,5 +1,19 @@
 package co.edu.unbosque.view;
 
+import java.util.Scanner;
+
 public class View {
+    private final Scanner consola;
+
+    public View() {
+        consola = new Scanner(System.in);
+
+    }
+
+    public int lectura() {
+        int numero = consola.nextInt();
+        consola.nextLine();
+        return numero;
+    }
 
 }
